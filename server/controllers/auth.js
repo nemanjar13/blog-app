d34be9af938b61ebd6421c96dbdd6203fef0157c
Nemanja Risteski <risteski.nemanja5@gmail.com>
2023-08-15 15:29:45 -0400

    Implemented registration

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,4 +1,31 @@
-export const register = (req, res) => {};
+import { db } from "../db.js";
+import bcrypt from "bcrypt";
+
+export const register = (req, res) => {
+  // check if user already exists
+  const query = "SELECT * FROM users WHERE email=? OR username=?";
+  db.query(query, [req.body.email, req.body.username], (err, data) => {
+    // if encounter error with the query
+    if (err) return res.json(err);
+
+    // if user already exists
+    if (data.length) return res.status(409).json("User already exists");
+
+    // if all is good, proceed with registration
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const query =
+      "INSERT INTO users(`username`, `email`, `password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
+
+    db.query(query, [values], (err, data) => {
+      if (err) return res.json(err);
+
+      return res.status(200).json("User has been created");
+    });
+  });
+};
 
 export const login = (req, res) => {};
 
--- server/index.js
+++ server/index.js
@@ -6,9 +6,9 @@ import authRoutes from "./routes/auth.js";
 const app = express();
 
 app.use(express.json());
-app.use("api/auth", authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
-app.use("api/users", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(8800, () => {
   console.log("Connected");
